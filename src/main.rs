fc2ea284fa4e01a8f57e4d937e0c38bb350d5637
sunjesse <jessera@outlook.com>
2025-01-25 21:36:29 -0500

    kinda works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+use std::process::Command;
 use trust_dns_resolver::Resolver;
 use trust_dns_resolver::config::*;
 
@@ -6,8 +8,8 @@ fn main() {
     let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
 
     let mut ips = Vec::with_capacity(blocked.len());
-    for addr in blocked {
-        let response = resolver.lookup_ip(addr).unwrap();
+    for addr in &blocked {
+        let response = resolver.lookup_ip(*addr).unwrap();
 
         let address = response.iter().next().expect("no addresses returned!");
          
@@ -17,4 +19,33 @@ fn main() {
     }
      
     println!("{:?}", ips);
+
+    let tmp_file_path = "/tmp/blocked_ips.txt";
+
+    fs::write(
+        tmp_file_path,
+        ips
+        .iter()
+        .map(|ip| ip.to_string())
+        .collect::<Vec<_>>()
+        .join("\n"),
+    )
+    .expect("Failed to write blocked IPs to file");
+
+    let add_command = Command::new("sudo")
+        .arg("pfctl")
+        .arg("-t")
+        .arg("blocked_ips")
+        .arg("-T")
+        .arg("add")
+        .arg("-f")
+        .arg(tmp_file_path)
+        .output();
+
+    match add_command {
+        Ok(out) if out.status.success() => println!("Successfully blocked the following websites: {:?}", blocked),
+        Ok(out) => eprintln!("Failed to update due to {:?}", String::from_utf8_lossy(&out.stderr)),
+        Err(e) => eprintln!("Failed to run command {:?}", e), 
+    }
+    
 }
