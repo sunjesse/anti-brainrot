eccc9dffe8f9f205b12594aeeee956c863b6ed0d
sunjesse <jessera@outlook.com>
2025-02-11 22:40:44 -0500

    block all ips

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,19 @@ use trust_dns_resolver::Resolver;
 use trust_dns_resolver::config::*;
 
 fn main() {
-    let blocked = vec!["www.facebook.com", "www.instagram.com", "www.tiktok.com"];
+    let blocked = vec!["www.reddit.com", "www.facebook.com", "www.instagram.com", "www.tiktok.com"];
     let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
 
     let mut ips = Vec::with_capacity(blocked.len());
     for addr in &blocked {
         let response = resolver.lookup_ip(*addr).unwrap();
 
-        let address = response.iter().next().expect("no addresses returned!");
+        for a in response.iter() {
+            ips.push(a);
+            println!("{:?} -> {:?}", addr, a);
+        }
          
-        println!("{:?} -> {:?}", addr, address);
         
-        ips.push(address);
     }
      
     println!("{:?}", ips);
